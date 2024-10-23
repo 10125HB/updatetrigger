0139970226d23cac430923f76d4ed7e387facb7b
10125HB <hannes.basler@gmail.com>
2024-10-23 20:34:53 +0000

    logging added

--- updatetest/model.py
+++ updatetest/model.py
@@ -1,3 +1,10 @@
+"""A simple model class with a decorator to call the update method after a method."""
+import logging
+
+
+logger = logging.getLogger()
+
+
 def call_update_after(method):
     """Decorator to call the update method after a method."""
     def wrapper(self, *args, **kwargs):
@@ -15,11 +22,12 @@ class Model:
         self._b: float = 2.0
         self._value = 0
         self.prevent_update = False
+        logger.info("Model created. Parameters: a=%s, b=%s, value=%s", self._a, self._b, self._value)
 
     def update(self):
         """Update the value of the model."""
         self._value += self._a + self._b
-        print(f"Value: {self._value}")
+        logger.info("Updated. Parameters: a=%s, b=%s, value=%s", self._a, self._b, self._value)
 
     @property
     def a(self):
@@ -30,3 +38,15 @@ class Model:
     @call_update_after
     def a(self, value):
         self._a = value
+        logger.info("Set a to %s", value)
+
+    @property
+    def b(self):
+        """The value of b"""
+        return self._b
+
+    @b.setter
+    @call_update_after
+    def b(self, value):
+        self._b = value
+        logger.info("Set b to %s", value)
